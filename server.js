f162c100bb0281a460917dc3b3444e9e403bff1b
negin-mgdm <negin.mgdm@gmail.com>
2024-07-07 23:58:58 +0100

    feat: Configuring web server to serve the homepage upon root url access.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8080;
 
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'homepage.html'));
 });
 
 app.listen(port, () => {
