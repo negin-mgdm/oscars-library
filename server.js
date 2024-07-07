61ad12bf4f8de7a027b29ab4c95bfcf9a2a96502
negin-mgdm <negin.mgdm@gmail.com>
2024-07-08 00:06:00 +0100

    feat: Adding new endpoint to return oscars.json data to client side.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 8080;
 
@@ -7,6 +8,18 @@ app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'homepage.html'));
+});
+
+app.get('/data', (req, res) => {
+    fs.readFile("oscars.json", 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading the data file.');
+            return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    });
 });
 
 app.listen(port, () => {
